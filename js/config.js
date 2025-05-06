1294754ee25a3fd8afd3fa030e3218a014dacd74
Echo-D26 <158822349+Echo-D26@users.noreply.github.com>
2025-05-06 14:59:47 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_GR5G4RQNj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2i535kc6qojroo65iijqo4ni0k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
